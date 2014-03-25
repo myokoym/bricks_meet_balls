3ab0e0e3d1b44527bfedca30f3c19b21829e8835
Masafumi Yokoyama <myokoym@gmail.com>
2014-03-26 00:30:20 +0900

    Add missing require

--- lib/bricks_meet_balls.rb
+++ lib/bricks_meet_balls.rb
@@ -4,6 +4,7 @@ require "bricks_meet_balls/brick"
 require "bricks_meet_balls/ball"
 require "bricks_meet_balls/bar"
 require "bricks_meet_balls/message"
+require "bricks_meet_balls/util"
 
 module BricksMeetBalls
 end
